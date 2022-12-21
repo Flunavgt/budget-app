bbbe352129a01b60b19e4392fee44bd796c52803
Federico Luna <fluna1973@yahoo.com>
2022-12-20 22:04:26 -0600

    refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,9 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer
     default_parameters = %i[name role]
     devise_parameter_sanitizer.permit(:sign_up, keys: default_parameters)
     devise_parameter_sanitizer.permit(:sign_in, keys: default_parameters)
     devise_parameter_sanitizer.permit(:account_update, keys: default_parameters)
-    # .permit(:sign_up, keys: [:name])
   end
 end
